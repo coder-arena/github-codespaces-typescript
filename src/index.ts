f8f7cf263d906a5b8984b86f023ec133c33f2b99
Marcos V. Leal <marcosleal.prd@gmail.com>
2020-11-22 03:59:07 +0000

    Adds express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Nenhuma implementação ainda...');
+import express from "express"
+
+const app = express()
+const port = 3000
+
+app.get("/", (req: express.Request, resp: express.Response) => {
+    resp.json({"message": "Subscribe to the channel."})
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
